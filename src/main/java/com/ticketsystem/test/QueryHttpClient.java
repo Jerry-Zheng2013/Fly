a821be3c10880998ba285ef9403c9a206fbfcd28
Jerry-Zheng2013 <zj20131112@163.com>
2021-07-06 01:55:26 +0800

    还剩验证客户状态不通过

--- src/main/java/com/ticketsystem/test/QueryHttpClient.java
+++ src/main/java/com/ticketsystem/test/QueryHttpClient.java
@@ -1,14 +1,17 @@
 package com.ticketsystem.test;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -25,17 +28,69 @@ public class QueryHttpClient {
 		String urlStr = "https://higo.flycua.com/ffp/member/login";
 		String requestType = "POST";
 		String dataStr = "NrcZ9YVVM/N5PuZaHJfqltN6wPGOIfrHwwFNJ4DTKCHb/yyON/GL4rvJVQxNcX1J9Ui22MPCTWHxcaNCrUiOmdhfbhpKhRZPxIegcZRKs7OCN1CCNH1QMiZZxgP5bdFD";
+		
+		try {
+			JSONObject logInPost = GetPostTest.sendPost(urlStr, dataStr);
+			//登录成功后，获取tokenId和tokenUUID
+			String tokenId = logInPost.getString("tokenId");
+			String tokenUUID = logInPost.getString("tokenUUID");
+			String accountCheckCookie2 = Test2.getAccountCheckCookie(tokenId, tokenUUID);
+			
+			
+			JSONObject queryPost = GetPostTest.sendGet2(
+	        		"http://www.flycua.com/app/searchFlights/queryFlight",
+	        		"_=1625491297316&org=CITY_BJS_CN&des=CitCnSHANGHA364&type=oneway&depd=2021-07-30&cals=false&adt=1&chd=0&gm=0&jc=0",
+	        		accountCheckCookie2
+	        		);
+			String session = queryPost.getString("session");
+	        System.out.println("session222="+session);
+
+			
+	        String bookCookie = Test2.getBookCookieData(session, tokenId, tokenUUID);
+	        
+			JSONObject queryPost3 = GetPostTest.sendGet2(
+	        		"http://www.flycua.com/app/login/queryUserStatus",
+	        		"_=0.6013036610715838",
+	        		bookCookie
+	        		);
+			String session3 = queryPost3.getString("session");
+	        System.out.println("session333="+session3);
+			
+	        /*
+	        String bookPostData = Test2.getBookPostData(tokenUUID);
+	        GetPostTest.sendPost2(
+	        		"http://www.flycua.com/app/booking/book?_=1625384445626", 
+	        		bookPostData, 
+	        		bookCookie);
+	        */
+	        
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/**
 		JSONObject logInPost = QueryHttpClient.logInPost(urlStr, requestType, dataStr);
 		//登录成功后，获取tokenId和tokenUUID
 		String tokenId = logInPost.getString("tokenId");
 		String tokenUUID = logInPost.getString("tokenUUID");
 		
+		String accountCheckCookie2 = Test2.getAccountCheckCookie(tokenId, tokenUUID);
+		JSONObject logInPost2 = QueryHttpClient.queryUserStatusPost(
+				"http://www.flycua.com/app/searchFlights/queryFlight?_=1625491297316&org=CITY_BJS_CN&des=CitCnSHANGHA364&type=oneway&depd=2021-07-30&cals=false&adt=1&chd=0&gm=0&jc=0", 
+				"GET", "","");
 		
+		
+		*/
+		
+		
+		
+		
+		/**
 		//检查账户状态
 		String urlStr2 = "http://www.flycua.com/app/login/queryUserStatus?_=0.6013036610715838";
 		String requestType2 = "GET";
 		String dataStr2 = "";
-		String accountCheckCookie = Test2.getAccountCheckCookie(tokenId, tokenUUID);
 		JSONObject userStatusPost = QueryHttpClient.queryUserStatusPost(urlStr2, requestType2, dataStr2, accountCheckCookie);
 		//查询账户状态成功之后，获取到session
 		String session = userStatusPost.getString("session");
@@ -51,10 +106,62 @@ public class QueryHttpClient {
 		System.out.println(jj);
 		String bookCookie = Test2.getBookCookieData(session, tokenId, tokenUUID);
 		QueryHttpClient.bookPost(urlStr3, requestType3, bookPostData, bookCookie);
-		
+		*/
 	}
 	
 
+	/**
+	* post方式连接获取cookie
+	* @param url 链接
+	* @param params 请求参数
+	* @param headers 请求头部参数
+	* @param charSet 编码
+	* @return cookie
+	*/
+	public static String post_cookie(String url, String params, Map<String, String> headers, String charSet) {
+		try {
+			URL url1 = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) url1.openConnection();
+			conn.setRequestMethod("POST");
+			// 输入参数
+			conn.setDoOutput(true);
+			// 不使用缓存
+			conn.setUseCaches(false);
+			conn.setInstanceFollowRedirects(false);
+			conn.setConnectTimeout(60 * 1000);
+			for (String header : headers.keySet()) {
+				conn.setRequestProperty(header, headers.get(header));
+			}
+			conn.connect();
+				DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+				String param = params;
+				out.writeBytes(param);
+				out.flush();
+				out.close();
+			String cookie = "";
+			Map<String, List<String>> respone_headers = conn.getHeaderFields();
+			for (String key : respone_headers.keySet()) {
+				// System.err.println(respone_headers.get(key));
+				if (key != null && key.equals("Set-Cookie")) {
+					List<String> string = respone_headers.get(key);
+					StringBuilder builder = new StringBuilder();
+					for (String str : string) {
+						System.err.println(str);
+						builder.append(str).toString();
+					}
+					cookie = builder.toString();
+				}
+			}
+			conn.disconnect();
+			return cookie;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	
+	
 	/**
 	 * 以post或get方式调用对方接口方法，
 	 * 
@@ -123,6 +230,24 @@ public class QueryHttpClient {
 			String session = originStr.substring(originStr.indexOf("JSESSIONID=")+11, originStr.indexOf("; Path=/; HttpOnly"));
 			String tokenId = originStr.substring(originStr.indexOf("tokenId=")+8, originStr.indexOf("Domain=.flycua.com; Path=/, JSESSIONID"));
 			
+			String cookie="";
+			   Map<String,List<String>> respone_headers=conn.getHeaderFields();
+			   for (String key : respone_headers.keySet()) {
+			   //System.err.println(respone_headers.get(key));
+			   if(key!=null &&key.equals("Set-Cookie")) {
+			       List<String> string = respone_headers.get(key);
+			       StringBuilder builder = new StringBuilder();
+			       for (String s1 : string) {
+			           System.err.println(s1);
+			           builder.append(s1).toString();
+			       }
+			       cookie=builder.toString();
+			   }
+			   }
+			System.out.println(cookie);
+			//post_cookie(urlStr, dataStr, headerFields, "UTF-8");
+			
+			
 			// 关闭流
 			is.close();
 			// 断开连接，disconnect是在底层tcp socket链接空闲时才切断，如果正在被其他线程使用就不切断。
@@ -181,7 +306,7 @@ public class QueryHttpClient {
 			conn.setRequestProperty("connection", "close");
 			conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36");
 			conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-			conn.addRequestProperty("Cookie", "asdf");
+			conn.setRequestProperty("Cookie", accountCheckCookie);
 			//conn.addRequestProperty(key, value);
 			
 			// DoOutput设置是否向httpUrlConnection输出，DoInput设置是否从httpUrlConnection读入，此外发送post请求必须设置这两个
